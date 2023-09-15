08d66106a1df829a30f244443ff7f2a7ca17a3d1
s-jad <s_jad@web.de>
2023-09-15 15:02:15 +0200

    Fix: input-label styling

--- form-with-validation/src/form.js
+++ form-with-validation/src/form.js
@@ -12,41 +12,41 @@ export default function SignUpForm() {
       <legend class="form-title">Sign up for more great features!</legend>
       <div class="form-input-container">
         <label for="name">
+          <input type="text" id="name" name="name" required maxlength="30" placeholder=""/>
           <div class="input-hint-bg">Name:</div>
           <div class="input-hint">Name:</div>
-          <input type="text" id="name" name="name" required maxlength="30"/>
           <div class="error" aria-live="polite"></div>
         </label>
       </div>       
       <div class="form-input-container">
         <label for="age">
+          <input type="number" id="age" name="age" required min="18" max="120" placeholder=""/>
           <div class="input-hint-bg">Age:</div>
           <div class="input-hint">Age:</div>
-          <input type="number" id="age" name="age" required min="18" max="120"/>
           <div class="error" aria-live="polite"></div>
         </label>
       </div>       
        <div class="form-input-container">
         <label for="mail">
+          <input type="email" id="mail" name="mail" required minlength="8" placeholder=""/>
           <div class="input-hint-bg">Email address:</div>
           <div class="input-hint">Email address:</div>
-          <input type="email" id="mail" name="mail" required minlength="8"/>
           <div class="error" aria-live="polite"></div>
         </label>
       </div>       
        <div class="form-input-container">
         <label for="password">
+          <input type="password" id="password" name="password" required minlength="12" placeholder=""/>
           <div class="input-hint-bg">Password:</div>
-            <div class="input-hint">Password:</div>
-          <input type="password" id="password" name="password" required minlength="12"/>
+          <div class="input-hint">Password:</div>
           <div class="error" aria-live="polite"></div>
         </label>
       </div>       
        <div class="form-input-container">
         <label for="confirm-password">
+          <input type="password" id="confirm-password" name="confirm-password" required minlength="12" placeholder=""/>
           <div class="input-hint-bg">Password confirm:</div>
           <div class="input-hint">Password confirm:</div>
-          <input type="password" id="confirm-password" name="confirm-password" required minlength="12"/>
           <div class="error" aria-live="polite"></div>
         </label>
       </div>       
